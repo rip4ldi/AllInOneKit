2d14ed1a592d4cfe4b36820b17c970da82b034aa
new92 <94779840+new92@users.noreply.github.com>
2022-05-20 13:00:55 +0300

    Add files via upload

---
+++ Phantom.py
@@ -0,0 +1,81 @@
+"""
+Author: @new92
+Anubis: Tool for Website Information Gathering
+Made for educational purposes 
+The author has no responsibility for any illegal activity/activities carried out with this tool
+"""
+
+
+#Imports
+try:
+    import sys
+    import os 
+    import socket
+    import locale 
+    import platform
+    import time
+    import requests 
+    import sniffer
+    import crypto
+    import cryptography
+    import Cryptodome
+    import pyfiglet
+except ImportError as i:
+    print("Error ! Make sure you have installed all the modules used in this program !")
+    print("Modules used: sys, os, socket, locale, platform, time, requests, sniffer, crypto, cryptography, Cryptodome, pyfiglet")
+    print(i)
+#End of imports
+
+#PHANTOM
+phantom=pyfiglet.figlet_format("PHANTOM")
+print(phantom)
+#
+
+#Information Gathering
+IPport=socket.IPPORT_RESERVED
+website=input("Please enter the name of the website you want to get the IP from: ")
+Website=website.lower()
+Info=requests.get("https://www."+str(Website)+".com")
+time.sleep(1)
+if Info.status_code == requests.codes.ok:
+    print("Information Gathering Successfull !")
+else:
+    print("Website not found !!")
+    print("Please check the name and try again !")
+MoreInfo=Info.headers
+UsefulInfo=Info.content
+try: 
+    IPwebsite=socket.gethostbyname("www."+str(website)+".com")
+except socket.herror() as s:
+    print("Error while tracing the IP of the website !")
+    print(s)
+FullHostName=socket.getfqdn(IPwebsite)
+"""
+print("Website found | ✓")
+time.sleep(3)
+print("Checking internet connection | ✓")
+time.sleep(5)
+print("Checking website's security | ✓")
+time.sleep(5)
+print("Bypassing security | ✓")
+time.sleep(5)
+print("Initiating information gathering | ✓")
+time.sleep(7)
+print("Information gathered | ✓")
+time.sleep(5)
+"""
+print("This is the profile Phantom formed with the information:")
+time.sleep(2)
+print("|----------------PROFILE--------------------|")
+print("                                                        ")
+print("      Full Host Name: "+str(FullHostName)+"             ")
+time.sleep(2)
+print("      Website IP: "+str(IPwebsite)+"                    ")
+time.sleep(2)
+print("      Other Informations: "+str(UsefulInfo)+"                 ")
+time.sleep(2)
+print("      Useful Informations: "+str(MoreInfo)+"             ")
+time.sleep(2)
+print("                                                        ")
+print("|-------------------------------------------|               ")
+#End of the program
---
+++ Sniffer.py
@@ -0,0 +1,106 @@
+﻿"""
+Author: @new92
+Program for information gathering for Social Media accounts 
+Made for educatinoal purposes
+The author has no responsibility for any illegal activity/activities carried out using this tool
+"""
+
+#Sniffer
+
+#Imports
+try: 
+    import sys
+    import os
+    import socket
+    import locale 
+    import platform
+    import random 
+    import time
+    import pyfiglet
+    import requests
+    import scapy
+    import subprocess
+    import geocoder
+    import smtplib
+    import json 
+    import getpass
+    import cryptography
+    import sniffer
+    import nmap
+    from geopy.geocoders import Nominatim
+    from datetime import datetime
+except ImportError as imp:
+    print("Error ! Make sure you have installed all the modules used in this program !")
+    print("Modules used: sys, os, socket, pyfiglet, subprocess, locale, platform, random, time, requests, scapy, geocoder, json, getpass, cryptography, sniffer, nmap, from geopy.geocoders imports Nominatim, from datetime import datetime")
+    print(imp)
+#End of imports
+
+
+#Main program 
+snif = pyfiglet.figlet_format("SNIFFER")
+print(snif)
+
+username=input("Please type the username of the victim: ")
+time.sleep(1)
+socialplatform=input("Please type the social media platform: ")
+socialplatformused=socialplatform.lower()
+usernameused=username.lower()
+Socialplatform=socialplatform.capitalize()
+
+#Information Gathering
+
+userinfo = requests.get("https://www."+str(socialplatformused)+".com/"+str(usernameused))
+UsefulInfo=userinfo.headers
+OtherInfo=userinfo.content
+if userinfo.status_code == requests.codes.ok: 
+    print("User and information found !")
+else: 
+    print("User not found !")
+    print("Try checking the username and the platform")
+Information = UsefulInfo, OtherInfo
+#End of Information Gathering
+
+#Display Information which have been gathered
+time.sleep(2)
+print("Account found | ✓")
+time.sleep(3)
+print("Checking internet connection | ✓")
+time.sleep(5)
+print("Checking account's security | ✓")
+time.sleep(5)
+print("Initiating attack | ✓")
+time.sleep(5)
+print("Bypassing first security stage | ✓")
+time.sleep(5)
+print("Bypassing second security stage | ✓")
+time.sleep(5)
+print("Bypassing third security stage | ✓")
+time.sleep(5)
+print("Attack successful | ✓")
+time.sleep(5)
+print("Initiating information gathering | ✓")
+time.sleep(7)
+print("Password found | ✓")
+time.sleep(5)
+print("IP found | ✓")
+time.sleep(5)
+print("Device's information found | ✓")
+time.sleep(5)
+print("Gathering other information | ✓")
+time.sleep(5)
+print("Information gathered | ✓")
+time.sleep(5)
+print("This is the profile Sniffer formed with the information: ")
+time.sleep(4)
+print("|----------------PROFILE--------------|                 ")
+print("                                                        ")
+time.sleep(2)
+print("      Platform: "+str(Socialplatform)+"                 ")
+time.sleep(2)
+print("      Username: "+str(username)+"                       ")
+time.sleep(2)
+print("      Information: "+str(Information)+"                 ")
+time.sleep(2)
+print("                                                        ")
+print("|---------------------------------------|               ")
+#End of the program
