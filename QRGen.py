4a66f638aadf0cd441efd4124abc9c30a317fa13
new92 <94779840+new92@users.noreply.github.com>
2022-06-03 20:54:37 +0300

    Add files via upload

---
+++ QRGen.py
@@ -0,0 +1,32 @@
+#Imports 
+import qrcode
+import time
+#End of Imports
+
+#Main program
+times=int(input("How many QR codes do you want to make ? "))
+if times == 1:
+    website=input("Please enter the link for the QR code: ")
+    time.sleep(2)
+    link=""+str(website)
+    qrC = qrcode.QRCode(version = 2,
+                    box_size = 10,
+                    border = 10)
+    qrC.add_data(link)
+    qrC.make(fit=True)
+    img = qrC.make_image(fill = "black")
+    img.save("qrcode.png")
+    print("Successfully saved at: qrcode.png")
+else: 
+    for i in range(int(times)):
+        website=input("Please enter the link for the number "+str(i + 1)+" QR code: ")
+        time.sleep(2)
+        link=""+str(website)
+        qrC = qrcode.QRCode(version = 2,
+                        box_size = 10,
+                        border = 10)
+        qrC.add_data(link)
+        qrC.make(fit=True)
+        QRimg= qrC.make_image(fill = "black")
+        QRimg.save("qrcode0000"+str(i)+".png")
+        print("Successfully saved at: qrcode.png")
